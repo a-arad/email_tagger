80c12a57b95930600e1388afa7c7c70943fba0d3
aradmschool <arad.n@masterschool.com>
2024-05-21 18:25:43 -0400

    add docstring

--- mailbox_to_df.py
+++ mailbox_to_df.py
@@ -2,27 +2,28 @@ import email
 import pandas as pd
 
 def mailbox_to_df(my_mail,data):
+    """put email plaintexts, to, and from into dicts then form data frame from list of dicts"""
+    
     emails = []
 
     for num in data[0].split():
-        typ, msg_data = my_mail.fetch(num, '(RFC822)')
+        _, msg_data = my_mail.fetch(num, '(RFC822)')
         for response_part in msg_data:
             if isinstance(response_part, tuple):
-                # Parse the email
+                
                 msg = email.message_from_bytes(response_part[1])
                 mail_from = msg.get('From')
                 mail_subject = msg.get('Subject')
                 mail_content = ''
 
-                # Check if the email message is multipart
                 if msg.is_multipart():
                     for part in msg.walk():
                         ctype = part.get_content_type()
                         cdispo = str(part.get('Content-Disposition'))
 
-                        # Look for plain text parts, but skip attachments
+                        # skip attachments
                         if ctype == 'text/plain' and 'attachment' not in cdispo:
-                            charset = part.get_content_charset('utf-8')  # default to utf-8
+                            charset = part.get_content_charset('utf-8') 
                             payload = part.get_payload(decode=True)
                             try:
                                 mail_content = payload.decode(charset)
@@ -30,20 +31,19 @@ def mailbox_to_df(my_mail,data):
                                 mail_content = payload.decode(charset, 'replace')  # replace undecodable chars
                             break
                 else:
-                    charset = msg.get_content_charset('utf-8')  # default to utf-8
+                    charset = msg.get_content_charset('utf-8') 
                     payload = msg.get_payload(decode=True)
                     try:
                         mail_content = payload.decode(charset)
                     except UnicodeDecodeError:
-                        mail_content = payload.decode(charset, 'replace')  # replace undecodable chars
+                        mail_content = payload.decode(charset, 'replace')
 
-                # Store data in list as dictionary
+                # 
                 emails.append({
                     'From': mail_from,
                     'Subject': mail_subject,
                     'Content': mail_content
                 })
 
-    # Create DataFrame from list of dictionaries
         email_df = pd.DataFrame(emails)
         return email_df
